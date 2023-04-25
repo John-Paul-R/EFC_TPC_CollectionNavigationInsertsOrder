2b9feff895665576684554c65ebe3d786d8010ae
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-04-25 08:52:07 -0400

    migration: database generated pkeys

--- EFC_TPC_CollectionNavigationInsertsOrder/Migrations/20230425125144_DatabaseGeneratedPKeys.Designer.cs
+++ EFC_TPC_CollectionNavigationInsertsOrder/Migrations/20230425125144_DatabaseGeneratedPKeys.Designer.cs
@@ -2,6 +2,7 @@
 using EFSampleApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFC_TPC_CollectionNavigationInsertsOrder.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20230425125144_DatabaseGeneratedPKeys")]
+    partial class DatabaseGeneratedPKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
---
+++ EFC_TPC_CollectionNavigationInsertsOrder/Migrations/20230425125144_DatabaseGeneratedPKeys.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFC_TPC_CollectionNavigationInsertsOrder.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseGeneratedPKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "MartialSkills",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "MagicSkills",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "MartialSkills",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "MagicSkills",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
